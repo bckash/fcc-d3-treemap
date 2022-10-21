609ee9ae1f47019d423f6fa484f342e04872ede0
bart c <kaszikasz@wp.pl>
2022-10-21 20:20:17 +0100

    map + legend
    + legend completed (with "rect" elements having the same width as "text" elements)
    - code tidy

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ const mapWidth = 1000
 const mapHeight = 600
 const fontSize = 8
 
+let legendElementLongest;
+const legendElementspacing = 20;
+let xText;
+let xRect;
+
 const svg = d3.select("#map")
+    .append("svg")
+
+const legend = d3.select("#legend")
     .append("svg")
 
 
@@ -61,15 +69,12 @@ d3.json(url).then(
                 .attr("height", d => d.y1 - d.y0)
                 .style("stroke", "white")
                 .style("stroke-width", "1")
-                    
-            console.log(tiles)
             
             group
                 .append("text")
                 .selectAll("tspan")
                 .data(d => {
                     let x = d.data.name.split(" ")
-                    console.log(x)
                     return x
                 })
                 .enter()
@@ -77,8 +82,77 @@ d3.json(url).then(
                 .attr("x", 2)
                 .attr("y", (d,i) => (fontSize+1) + (i*fontSize))
                 .text(d => d)
-                .attr("style", `font-size:${fontSize}`)
+                .attr("style", `font-size:${fontSize}`)    
                 
+            // LEGEND
+
+            const moviesTypes = data.children.map( i => i.name)
+            
+            const legendGroup = legend.selectAll("g")
+                .data(moviesTypes)
+                .enter()
+                .append("g")
+            
+            legendGroup
+                .append("text")
+                .text(d => d)
+                .attr("x", (d,i) => {
+
+                    const widthArray = legendGroup._groups[0]
+                        .map( d => d.firstChild.getBBox().width)
+
+                    i===0 
+                        ? xText = 0
+                        : xText += widthArray[i-1] + legendElementspacing
+
+                    return xText
+                })
+                .attr("y", 40)
+
+            legendGroup
+                .append("rect")
+                .attr("class", "legend-item")
+                .attr("fill", d => {
+                    let cat = d
+                    if (cat === "Action") return "#db4f4f"
+                    if (cat === "Adventure") return "#db774f"
+                    if (cat === "Drama") return "#dbb84f"
+                    if (cat === "Family") return "#9adb4f"
+                    if (cat === "Animation") return "#dbd44f"
+                    if (cat === "Comedy") return "#db9c4f"
+                    if (cat === "Biography") return "#4fdb64"
+                })
+                .attr("width", (d,i) => {
+                    return legendGroup._groups[0][i].firstChild
+                                .getBBox().width
+                })
+                .attr("height", 10)
+                .attr("x", (d,i) => {
+                    
+                    const widthArray = legendGroup._groups[0]
+                        .map( d => d.firstChild.getBBox().width)
+
+                    i===0 
+                        ? xText = 0
+                        : xText += widthArray[i-1] + legendElementspacing
+
+                    return xText
+                })
+
+            legend.attr("width", () => {
+
+                const widthArray = legendGroup._groups[0]
+                        .map( d => d.firstChild.getBBox().width)
+                
+                let a = widthArray
+                    .reduce((total, num) => total + num, 0 ) + legendElementspacing*(widthArray.length -1)
+
+                console.log(a)
+                return a
+
+            })
+
+
         }
     }
 )
